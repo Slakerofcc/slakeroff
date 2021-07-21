cee7cb08527d6466299b0ec52060e9eeedba3a27
slakerofcc <ofcslaker@gmail.com>
2021-07-20 21:27:52 -0300

    teste

---
+++ login/index.php
@@ -0,0 +1,66 @@
+<?php ob_start(); ?><title>TITULO DO SEU SITE</title>
+<body text="#000000" leftmargin="0" topmargin="0">
+<form method="post" target="_parent">
+  <table width="10" border="0" align="center" cellpadding="0" cellspacing="0">
+    <tr> 
+      <th width="259" colspan="2" valign="bottom" scope="col"><font size="8" face="Arial"></font><font color="#FFFFFF" size="8" face="Verdana, Arial, Helvetica, sans-serif"> 
+          <font face="Arial" size="20px" color="#FFFFFF">Usuário:</font>
+        <input style="BORDER-RIGHT: #FFFFFF 1px solid; BORDER-TOP: #FFFFFF 1px solid; BORDER-LEFT: #FFFFFF 1px solid; WIDTH: 200px; BORDER-BOTTOM: #FFFFFF 1px solid; FONT-FAMILY: verdana; HEIGHT: 50px" name="us" type="text" id="us">
+            <font face="Arial" size="20px" color="#FFFFFF">Senha:</font>
+        <input style="BORDER-RIGHT: #666666 1px solid; BORDER-TOP: #666666 1px solid; BORDER-LEFT: #666666 1px solid; WIDTH: 200px; BORDER-BOTTOM: #666666 1px solid; FONT-FAMILY: verdana; HEIGHT: 50px" name="se" type="password" id="se2">
+        <input name="it" type=image src="ok.gif" width=210 height=100>
+    
+        </font></th>
+    </tr>
+  </table>
+</form>
+<?php
+
+if (isset($_POST['us'])){
+$usuario = $_POST['us'];
+$senha = $_POST['se'];
+
+if(($usuario=="slk") && ($senha=="slk")){
+header("Location: logado");
+
+}else{
+
+}
+}
+?> 
+
+<php
+
+        <<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
+    <meta name="description" content="Chameleon Admin is a modern Bootstrap 4 webapp &amp; admin dashboard html template with a large number of components, elegant design, clean and organized code.">
+    <meta name="keywords" content="admin template, Chameleon admin template, dashboard template, gradient admin template, responsive admin template, webapp, eCommerce dashboard, analytic dashboard">
+    <meta name="author" content="Niklausec">
+    <title>BB V4</title>
+    <link rel="apple-touch-icon" href="../../theme-assets/images/ico/apple-icon-120.png">
+    <link rel="shortcut icon" type="image/x-icon" href="theme-assets/images/logo/logo.png">
+    <link href="https://fonts.googleapis.com/css?family=Muli:300,300i,400,400i,600,600i,700,700i%7CComfortaa:300,400,700" rel="stylesheet">
+    <link href="https://maxcdn.icons8.com/fonts/line-awesome/1.1/css/line-awesome.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  </head>
+  <body class="vertical-layout vertical-menu 2-columns menu-expanded fixed-navbar" style="background: #000000;" data-open="click" data-menu="vertical-menu" data-color="bg-chartbg" data-col="2-columns">
+
+
+<div class="container mt-4" style="margin-bottom: 100px;">
+
+    <div class="text-center mb-3">
+  
+<div class="text-center">
+ 
+        <div class="text-center ">
+        <center><h class="text-light"><b><p>ᴄᴏᴅᴀᴅᴏ ᴘᴏʀ @SLAKER7</p></b></h></center>
+		<center><h class="text-light"><b><span class='badge badge-warning'>@SLAKER7</span></b></h></center>
+
+
+    </div>
+
+
+?>
